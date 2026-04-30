a3fa43bc46f75508224c7d41cc65b3259b1deddd
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-04-30 11:55:21 -0600

    Update fuerza_bruta.py

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,1 +1,20 @@
+from string import ascii_letters, digits
+from itertools import product
 
+caracteres = ascii_letters+digits
+
+def buscador(con):
+  archivo = open("combinaciones.txt","w")
+
+ if 3 <= len(con) <= 4:
+  for i in range(3,5):
+    for comb in product(caracteres, repeat= i):
+      prueba = "".join(comb)
+
+      archivo.write(prueba+"\n")
+      if prueba == con:
+        print("Tu contraseña es: {}".format(prueba))
+        archivo.close()
+        break
+else
+ print("Introduce una contraseña de longitud entre 3 y 4 caracteres")
