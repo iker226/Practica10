6d70d01844a63988326d582d4a2a7f555de7432b
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-04-30 11:56:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from time import time
+from fuerza_bruta import buscador
 
+to = time()
+con = 'H014'
+buscador(con)
+t1 = time()
+t2 = round(t1-t0, 6)
+print("El tiempo de ejecucion fue {}".format(t2))
