133d684a473c79fefc2679f2aaeb85fd5699206c
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-04-30 12:03:19 -0600

    Update actividades_greedy.py

--- actividades_greedy.py
+++ actividades_greedy.py
@@ -1,1 +1,21 @@
+actividades = [
+  ("A", 1, 4),
+  ("B", 3, 5),
+  ("C", 0, 6),
+  ("D", 5, 7),
+  ("E", 8, 9),
+  ("F", 5, 9)
+]
+  
+actividades.sort(key=lambda x:x[2])
 
+seleccionadas= []
+fin_actual = 0
+
+for actividad in actividades:
+  nombre, inicio, fin = actividad
+  if inicio >= fin_actual:
+    seleccionadas.append(nombre)
+    fin_actual = fin
+
+print("Actividades seleccionadas ", seleccionadas)
