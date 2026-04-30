72527d0ca1808265dcb9c03be84f33975363e745
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-04-30 12:00:56 -0600

    Update cambio.py

--- cambio.py
+++ cambio.py
@@ -1,1 +1,19 @@
+from string import ascii_letters, digits
+from itertools import product
 
+def cambio(cantidad, denominaciones) :
+  resultado = []
+  while (cantidad > 0):
+    if (cantidad >= denominaciones [0]) :
+
+      num = cantidad // denominaciones[0]
+      cantidad = cantidad - (num * denominaciones[0])
+      resultado.append( [denominaciones [0], num] )
+    denominaciones = denominaciones [1:]
+  return resultado
+
+print(cambio(1000, [500, 200, 100, 50, 20, 5, 1]) )
+print(cambio(500, [500, 200, 100, 50, 20, 5, 1]) )
+print(cambio(300, [50, 20, 5, 1]))
+print(cambio(200, [5]))
+print(cambio(98, [50, 20, 5,1]))
